a06937e27d914014796d85716c6004f38c1bcce8
Atharva <deshmukhsambhajirao9@gmail.com>
2024-09-11 17:19:51 +0530

    init

---
+++ src/app/api/transfer-sol/const.ts
@@ -0,0 +1,8 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const DEFAULT_SOL_ADDRESS: PublicKey = new PublicKey(
+  "AE7SJcQbRsLwAWbu9tBKhLaGMwRRXUSvXtfkma8agaSF", 
+  //Replace it with your Solana wallet address
+);
+
+export const DEFAULT_SOL_AMOUNT: number = 1.0;
